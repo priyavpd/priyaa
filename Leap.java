3ee9ad6d2cd115a11dc6b1d60b9ef4b22f1faea1
priyavpd <vpriyacse2@gmail.com>
2016-07-22 15:25:17 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,16 @@
+# priyaa
+public class Leap
+{
+public static void main(String[] args)
+{
+int n=2004;
+if(n%4==0 || n%400==0 || n%100!=0)
+{
+System.out.println("leap year");
+}
+else
+{
+System.out.println("leap not year");
+}
+}
+}
